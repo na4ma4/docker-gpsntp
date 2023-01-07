3bf57358390d8a9f3da466ea1535788258002e40
na4ma4 <na4ma4@users.noreply.github.com>
2023-01-07 14:43:58 +1000

    updated gps-server

---
+++ gpsd/init.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+rm /dev/ttyAMA0 /dev/pps0
+
+ln -s /host/dev/ttyAMA0 /dev/ttyAMA0
+ln -s /host/dev/pps0 /dev/pps0
+ln -s /host/dev/gps* /dev/
+
+/sbin/syslogd -S -O - -n &
+
+exec /usr/sbin/gpsd -N -n -r -G -D ${DEBUG_LEVEL:-0} --sockfile /run/gpsd.sock ${GPS_DEVICE} --
